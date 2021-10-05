c8580d87c98ffe8fc62f296606d24cf65be37010
Victor Otieno <victorotieno598@gmail.com>
2021-10-06 02:13:32 +0300

    Add table to display missions

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,5 +1,36 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Missions = () => (<div className="mission-page">Welcome to mission page.</div>);
+const Missions = () => {
+  const missions = useSelector((state) => state.missions);
+
+  return (
+    <div className="mission-page">
+      <table className="missions-table">
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.length > 0 ? missions.map((mission) => (
+            <tr key={mission.id}>
+              <td><b>{mission.name}</b></td>
+              <td>{mission.description}</td>
+              <td>
+                <button type="button" className={mission.joined ? 'joined-mission' : 'unjoined-mission'}>
+                  {mission.joined ? 'Leave' : 'Join'}
+                  {' '}
+                  Mission
+                </button>
+              </td>
+            </tr>
+          )) : 'No Missions'}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Missions;
