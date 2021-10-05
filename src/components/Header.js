61653ee9f96e448e5680ef00d3d748f970f0544f
HENRY-KC <henrykc24@yahoo.com>
2021-10-05 09:01:32 +0100

    Add navbar

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+import Navbar from './Navbar';
+
+const Header = () => (
+  <header>
+    <Navbar />
+  </header>
+);
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Navbar = () => (<nav>This is the navigation bar</nav>);
+
+export default Navbar;
