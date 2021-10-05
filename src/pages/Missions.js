53a71afda609a6d5dcae6cd79a39ffac0621722a
Victor Otieno <victorotieno598@gmail.com>
2021-10-06 02:50:27 +0300

    Add more styling and fix linters

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,9 +1,8 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { joinMission } from '../redux/missions/missions';
+import { useSelector } from 'react-redux';
+import MissionTableRow from './MissionTableRow';
 
 const Missions = () => {
   const missions = useSelector((state) => state.missions);
-  const dispatch = useDispatch();
 
   return (
     <div className="mission-page">
@@ -17,17 +16,7 @@ const Missions = () => {
         </thead>
         <tbody>
           {missions.length > 0 ? missions.map((mission) => (
-            <tr key={mission.id}>
-              <td><b>{mission.name}</b></td>
-              <td>{mission.description}</td>
-              <td>
-                <button onClick={() => dispatch(joinMission(mission.id))} type="button" className={mission.joined ? 'joined-mission btn' : 'unjoined-mission btn'}>
-                  {mission.joined ? 'Leave' : 'Join'}
-                  {' '}
-                  Mission
-                </button>
-              </td>
-            </tr>
+            <MissionTableRow mission={mission} key={mission.id} />
           )) : 'No Missions'}
         </tbody>
       </table>
