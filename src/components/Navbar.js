282aeeff38c5a04f02f99df2a44039ba77db4d92
HENRY-KC <henrykc24@yahoo.com>
2021-10-06 17:52:10 +0100

    Add underline to active link and update reamde file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import Image from '../logo.png';
 const Navbar = () => (
   <nav className="navbar">
     <div className="app-name-logo">
-      <NavLink to="/" exact>
+      <NavLink style={{ border: 'none' }} to="/" exact>
         <img src={Image} alt="space x" width="60" height="60" />
       </NavLink>
-      <NavLink to="/" style={{ textDecoration: 'none', color: '#000' }}><h1 className="app-name">Space Travellers</h1></NavLink>
+      <NavLink to="/" style={{ textDecoration: 'none', border: 'none', color: '#000' }}><h1 className="app-name">Space Travellers</h1></NavLink>
     </div>
     <ul className="nav">
       <li>
